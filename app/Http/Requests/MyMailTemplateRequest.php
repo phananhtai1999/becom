42114f7b4e85a882e95b62142cccc72150556530
sonnguyentechupcorp <95366420+sonnguyentechupcorp@users.noreply.github.com>
2022-04-14 17:49:47 +0700

    SEM-40

---
+++ app/Http/Requests/MyMailTemplateRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Abstracts\AbstractRequest;
+use Illuminate\Validation\Rule;
+
+class MyMailTemplateRequest extends AbstractRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'subject' => ['required', 'string'],
+            'body' => ['required', 'string'],
+            'website_uuid' => ['required', 'numeric', 'min:1', Rule::exists('websites', 'uuid')->where(function ($query) {
+
+                return $query->where('user_uuid', auth()->user()->getkey())->whereNull('deleted_at');
+            })],
+        ];
+    }
+}
--- app/Services/MailTemplateService.php
+++ app/Services/MailTemplateService.php
@@ -8,4 +8,57 @@ use App\Models\MailTemplate;
 class MailTemplateService extends AbstractService
 {
     protected $modelClass = MailTemplate::class;
+
+    /**
+     * @param $perPage
+     * @return mixed
+     */
+    public function indexMyMailTemplate($perPage)
+    {
+        return $this->model->select('mail_templates.*')
+            ->join('websites', 'websites.uuid', '=', 'mail_templates.website_uuid')
+            ->join('users', 'users.uuid', '=', 'websites.user_uuid')
+            ->where('websites.user_uuid', auth()->user()->getKey())
+            ->paginate($perPage);
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function findMyMailTemplateByKeyOrAbort($id)
+    {
+        $smtpAccount = $this->model->select('mail_templates.*')
+            ->join('websites', 'websites.uuid', '=', 'mail_templates.website_uuid')
+            ->join('users', 'users.uuid', '=', 'websites.user_uuid')
+            ->where('websites.user_uuid', auth()->user()->getKey())
+            ->where('mail_templates.uuid', $id)
+            ->first();
+
+        if (!empty($smtpAccount)) {
+            return $smtpAccount;
+        } else {
+            abort(403, 'Unauthorized.');
+        }
+    }
+
+    /**
+     * @param $id
+     * @return mixed|void
+     */
+    public function deleteMyMailTemplateByKey($id)
+    {
+        $smtpAccount = $this->model->select('mail_templates.*')
+            ->join('websites', 'websites.uuid', '=', 'mail_templates.website_uuid')
+            ->join('users', 'users.uuid', '=', 'websites.user_uuid')
+            ->where('websites.user_uuid', auth()->user()->getKey())
+            ->where('mail_templates.uuid', $id)
+            ->first();
+
+        if (!empty($smtpAccount)) {
+            return $this->destroy($smtpAccount->getKey());
+        } else {
+            abort(403, 'Unauthorized.');
+        }
+    }
 }
