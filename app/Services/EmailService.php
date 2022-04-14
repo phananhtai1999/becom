d58dfd9ccac712ee87cac8c72f35568a6dc2d4b8
sonnguyentechupcorp <95366420+sonnguyentechupcorp@users.noreply.github.com>
2022-04-14 17:28:51 +0700

    SEM-38

--- app/Services/EmailService.php
+++ app/Services/EmailService.php
@@ -8,4 +8,57 @@ use App\Models\Email;
 class EmailService extends AbstractService
 {
     protected $modelClass = Email::class;
+
+    /**
+     * @param $perPage
+     * @return mixed
+     */
+    public function indexMyEmail($perPage)
+    {
+        return $this->model->select('emails.*')
+            ->join('websites', 'websites.uuid', '=', 'emails.website_uuid')
+            ->join('users', 'users.uuid', '=', 'websites.user_uuid')
+            ->where('websites.user_uuid', auth()->user()->getKey())
+            ->paginate($perPage);
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function findMyEmailByKeyOrAbort($id)
+    {
+        $smtpAccount = $this->model->select('emails.*')
+            ->join('websites', 'websites.uuid', '=', 'emails.website_uuid')
+            ->join('users', 'users.uuid', '=', 'websites.user_uuid')
+            ->where('websites.user_uuid', auth()->user()->getKey())
+            ->where('emails.uuid', $id)
+            ->first();
+
+        if (!empty($smtpAccount)) {
+            return $smtpAccount;
+        } else {
+            abort(403, 'Unauthorized.');
+        }
+    }
+
+    /**
+     * @param $id
+     * @return mixed|void
+     */
+    public function deleteMyEmailByKey($id)
+    {
+        $smtpAccount = $this->model->select('emails.*')
+            ->join('websites', 'websites.uuid', '=', 'emails.website_uuid')
+            ->join('users', 'users.uuid', '=', 'websites.user_uuid')
+            ->where('websites.user_uuid', auth()->user()->getKey())
+            ->where('emails.uuid', $id)
+            ->first();
+
+        if (!empty($smtpAccount)) {
+            return $this->destroy($smtpAccount->getKey());
+        } else {
+            abort(403, 'Unauthorized.');
+        }
+    }
 }
