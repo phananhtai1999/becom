b95cc4850092beeb8fe4da8aa342beba38409305
phananhtai1 <1751010128tai@ou.edu.vn>
2022-04-18 12:01:48 +0700

    SEM-52

---
+++ database/migrations/2022_04_18_045640_rename_scret_key_to_secret_key_in_smtp_accounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenameScretKeyToSecretKeyInSmtpAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('smtp_accounts', function (Blueprint $table) {
+            $table->renameColumn('scret_key', 'secret_key');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('smtp_accounts', function (Blueprint $table) {
+            $table->renameColumn('secret_key', 'scret_key');
+        });
+    }
+
+}
