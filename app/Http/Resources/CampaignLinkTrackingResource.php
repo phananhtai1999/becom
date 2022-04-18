e3d502a87cceaef0b87fde6d8b3e18424bd05291
Tai Phan <tai.phan@techupcorp.com>
2022-04-18 09:12:55 +0000

    SEM-57

--- app/Http/Resources/CampaignLinkTrackingResource.php
+++ app/Http/Resources/CampaignLinkTrackingResource.php
@@ -9,12 +9,15 @@ class CampaignLinkTrackingResource extends AbstractJsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-        return [
+
+        $expand = request()->get('expand', []);
+
+        $data = [
             'uuid' => $this->getKey(),
             'campaign_uuid' => $this->campaign_uuid,
             'to_url' => $this->to_url,
@@ -23,5 +26,11 @@ class CampaignLinkTrackingResource extends AbstractJsonResource
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at
         ];
+
+        if (\in_array('campaign_link_tracking__campaign', $expand)) {
+            $data['campaign'] = new CampaignResource($this->campaign);
+        }
+
+        return $data;
     }
 }
