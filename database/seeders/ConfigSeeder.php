4f1b8f78b406579a7b75407a8108fc341ec764ec
sonnguyentechupcorp <95366420+sonnguyentechupcorp@users.noreply.github.com>
2022-10-11 10:50:35 +0700

    Create ConfigSeeder

---
+++ database/seeders/ConfigSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Config;
+use Illuminate\Database\Seeder;
+
+class ConfigSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $key = [
+            [
+                'key' => 'stmp_auto',
+                'value' => true,
+            ],
+        ];
+
+        foreach ($key as $value) {
+            Config::firstOrCreate($value);
+        }
+    }
+}
