96e4ad0189d200079ef717f4784dc7b8dfa49922
nguyenhson112 <nguyenhson112@gmail.com>
2023-03-14 15:56:47 +0700

    SEM-837: Cast value to integer in config

--- app/Http/Controllers/Api/ConfigController.php
+++ app/Http/Controllers/Api/ConfigController.php
@@ -12,15 +12,12 @@ use App\Http\Resources\ConfigResourceCollection;
 use App\Http\Resources\ConfigResource;
 use App\Http\Controllers\Traits\RestIndexTrait;
 use App\Http\Controllers\Traits\RestShowTrait;
-use App\Http\Controllers\Traits\RestEditTrait;
-use App\Http\Controllers\Traits\RestStoreTrait;
 use App\Services\ConfigService;
-use http\Env\Request;
 use Illuminate\Http\JsonResponse;
 
 class ConfigController extends AbstractRestAPIController
 {
-    use RestIndexTrait, RestShowTrait, RestEditTrait, RestStoreTrait;
+    use RestIndexTrait, RestShowTrait;
 
     /**
      * @param ConfigService $service
@@ -33,6 +30,65 @@ class ConfigController extends AbstractRestAPIController
         $this->storeRequest = ConfigRequest::class;
         $this->editRequest = UpdateConfigRequest::class;
         $this->indexRequest = IndexRequest::class;
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function store()
+    {
+        $request = app($this->storeRequest);
+
+        // Cast value to integer
+        if ($request->type === 'boolean' || $request->type === 'number') {
+            if ($request->value == 0) {
+                $value = '00';
+                $castValue = (float)$value;
+            } else {
+                $castValue = (float)$request->value;
+            }
+        } else {
+            $castValue = $request->value;
+        }
+
+        $model = $this->service->create(array_merge($request->all(), [
+            'value' => $castValue
+        ]));
+
+        return $this->sendCreatedJsonResponse(
+            $this->service->resourceToData($this->resourceClass, $model)
+        );
+    }
+
+    /**
+     * @param $id
+     * @return JsonResponse
+     */
+    public function edit($id)
+    {
+        $request = app($this->editRequest);
+
+        $model = $this->service->findOrFailById($id);
+
+        // Cast value to integer
+        if ($request->type === 'boolean' || $request->type === 'number') {
+            if ($request->value == 0) {
+                $value = '00';
+                $castValue = (float)$value;
+            } else {
+                $castValue = (float)$request->value;
+            }
+        } else {
+            $castValue = $request->value;
+        }
+
+        $this->service->update($model, array_merge($request->all(), [
+            'value' => $castValue
+        ]));
+
+        return $this->sendOkJsonResponse(
+            $this->service->resourceToData($this->resourceClass, $model)
+        );
     }
 
     /**
@@ -69,7 +125,8 @@ class ConfigController extends AbstractRestAPIController
         );
     }
 
-    public function getCachePlatformConfig($id){
+    public function getCachePlatformConfig($id)
+    {
         $cache = $this->service->findOneWhere(['key' => $id . '_cache']);
 
         return $this->sendCreatedJsonResponse(
@@ -77,7 +134,8 @@ class ConfigController extends AbstractRestAPIController
         );
     }
 
-    public function editCachePlatformConfig($platformPackageUuid, UpdateCachePlatformConfig $request) {
+    public function editCachePlatformConfig($platformPackageUuid, UpdateCachePlatformConfig $request)
+    {
         $model = $this->service->findOneWhere(['key' => $platformPackageUuid . '_cache']);
         $this->service->update($model, $request->all());
 
