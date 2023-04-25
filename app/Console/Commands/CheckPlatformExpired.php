efb9845353c2c53a41e38653d9882f2be7cf599d
LeeC <88872059+leephp-c@users.noreply.github.com>
2023-04-25 14:31:12 +0700

    TEAM-18

--- app/Console/Commands/CheckPlatformExpired.php
+++ app/Console/Commands/CheckPlatformExpired.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\PlatformPackage;
 use App\Services\UserAddOnService;
 use App\Services\UserPlatformPackageService;
 use App\Services\UserService;
@@ -50,10 +51,14 @@ class CheckPlatformExpired extends Command
             }
         }
 
-        $userPlatformPackages = $this->userPlatformPackageService->findAllWhere([['platform_package_uuid' , '!=', 'stater']]);
+        $userPlatformPackages = $this->userPlatformPackageService->findAllWhere([['platform_package_uuid' , '!=', 'starter']]);
         foreach ($userPlatformPackages as $userPlatformPackage) {
             if($userPlatformPackage->expiration_date < Carbon::now()) {
                 $this->userPlatformPackageService->destroy($userPlatformPackage->uuid);
+                $this->userPlatformPackageService->create([
+                    'platform_package_uuid' => PlatformPackage::DEFAULT_PLATFORM_PACKAGE_1,
+                    'user_uuid' => $userPlatformPackage->user_uuid
+                ]);
             }
         }
     }
