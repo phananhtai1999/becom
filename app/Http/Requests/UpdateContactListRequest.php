1e25ea96bb9a7489accdb3d4fceff3d268595df4
sonnguyentechupcorp <95366420+sonnguyentechupcorp@users.noreply.github.com>
2023-06-05 16:48:58 +0700

    SEM-1414:Update ContactList request

--- app/Http/Requests/UpdateContactListRequest.php
+++ app/Http/Requests/UpdateContactListRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Abstracts\AbstractRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateContactListRequest extends AbstractRequest
 {
@@ -27,8 +28,10 @@ class UpdateContactListRequest extends AbstractRequest
             'file' => ['nullable', 'mimes:xlsx,csv,json,js'],
             'name' => ['string'],
             'contact' => ['nullable', 'array', 'min:1'],
-            'contact.*' => ['numeric', 'min:1', 'exists:contacts,uuid'],
-            'user_uuid' => ['numeric', 'min:1', 'exists:users,uuid'],
+            'contact.*' => ['numeric', 'min:1', Rule::exists('contacts', 'uuid')->where(function ($query) {
+                return $query->where('user_uuid', $this->request->get('user_uuid') ?? auth()->user()->getKey())->whereNull('deleted_at');
+            })],
+            'user_uuid' => ['nullable', 'numeric', 'min:1', Rule::exists('users', 'uuid')->whereNull('deleted_at')],
         ];
     }
 }
