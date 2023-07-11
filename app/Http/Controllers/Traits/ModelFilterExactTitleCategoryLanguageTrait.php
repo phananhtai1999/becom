82fd0927e26a44fa6f674ee6b8a287a4fa72b4a6
sonnguyentechupcorp <95366420+sonnguyentechupcorp@users.noreply.github.com>
2023-07-11 10:26:19 +0700

    SEM-1591: Add title_category filter for section and section category

---
+++ app/Http/Controllers/Traits/ModelFilterExactTitleCategoryLanguageTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+
+trait ModelFilterExactTitleCategoryLanguageTrait
+{
+    /**
+     * @param Builder $query
+     * @param ...$title
+     * @return Builder
+     */
+    public function scopeExactTitleCategory(Builder $query, ...$title)
+    {
+        return $query->where(function ($q) use($title){
+            $lang = app()->getLocale();
+            $langDefault = config('app.fallback_locale');
+            foreach ($title as $value) {
+                $q->orWhereRaw("IFNULL(JSON_UNQUOTE(JSON_EXTRACT(title, '$.$lang')),JSON_UNQUOTE(JSON_EXTRACT(title, '$.$langDefault'))) = '$value'");
+            }
+        });
+    }
+}
