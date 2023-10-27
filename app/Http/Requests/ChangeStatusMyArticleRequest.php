54341a3b4bf7d0f7b694524f0f1dde0caf7e3d89
CuongNguyen <88872059+leephp-c@users.noreply.github.com>
2023-10-27 17:24:53 +0700

    SEM-1830

---
+++ app/Http/Requests/ChangeStatusMyArticleRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Article;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ChangeStatusMyArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $validate = [
+            'articles' => ['required', 'array', 'min:1'],
+            'articles.*' => ['numeric', 'min:1', Rule::exists('articles', 'uuid')->where(function ($query) {
+                return $query->where([
+                    ['publish_status', '<>', $this->request->get('publish_status')]
+                ])->whereNull('deleted_at');
+            })],
+            'publish_status' => ['required', 'numeric', Rule::in(Article::PUBLISHED_PUBLISH_STATUS, Article::REJECT_PUBLISH_STATUS, Article::BLOCKED_PUBLISH_STATUS, Article::PENDING_PUBLISH_STATUS, Article::DRAFT_PUBLISH_STATUS)],
+            'content_for_user' => ['nullable', 'string', 'in:public,login,payment,editor,admin'],
+        ];
+
+        if ($this->request->get('publish_status') == Article::REJECT_PUBLISH_STATUS) {
+            $validate['reject_reason'] = ['required', 'string'];
+        }
+
+        return $validate;
+    }
+}
