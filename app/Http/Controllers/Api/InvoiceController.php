fd1d0c5a57da41b6b44228232f9c4435dd454984
CuongNguyen <88872059+leephp-c@users.noreply.github.com>
2024-01-17 16:06:39 +0700

    SEM-2123: FIX download invoice

--- app/Http/Controllers/Api/InvoiceController.php
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -35,7 +35,7 @@ class InvoiceController extends AbstractRestAPIController
         $paymentMethod = $invoice->paymentMethod;
         $date = new DateTime($invoice->created_at);
         $time = $this->service->getConfigByKeyInCache('timezone')->value;
-        $timezone = new DateTimeZone($time->value);
+        $timezone = new DateTimeZone($time);
         $date->setTimezone($timezone);
         $invoice->created_date = $date->format('d/m/Y H:i:s');
 
