9053d9e839c2a4ada32b5ef8c98c6a1b7b0c137d
CuongNguyen <88872059+leephp-c@users.noreply.github.com>
2024-01-18 17:06:27 +0700

    Fix get article seri

--- app/Http/Controllers/Api/ArticleSeriesController.php
+++ app/Http/Controllers/Api/ArticleSeriesController.php
@@ -52,7 +52,7 @@ class ArticleSeriesController extends AbstractRestAPIController
             return $this->sendValidationFailedJsonResponse();
         }
 
-        $model = $this->service->create(array_merge($request->except(['article_uuid']), ['app_id' => auth()->appId]));
+        $model = $this->service->create(array_merge($request->except(['article_uuid']), ['app_id' => auth()->appId()]));
 
         return $this->sendCreatedJsonResponse(
             $this->service->resourceToData($this->resourceClass, $model)
@@ -72,7 +72,7 @@ class ArticleSeriesController extends AbstractRestAPIController
         }
         $model = $this->service->findOrFailById($id);
 
-        $this->service->update($model, array_merge($request->except(['article_uuid']), ['app_id' => auth()->appId]));
+        $this->service->update($model, array_merge($request->except(['article_uuid']), ['app_id' => auth()->appId()]));
 
         return $this->sendOkJsonResponse(
             $this->service->resourceToData($this->resourceClass, $model)
